ac5455e63e99e0e7ff374ab374a059763d291110
AvanMyers <smail1341@gmail.com>
2022-08-11 17:17:36 +0300

    pluse

---
+++ plusSeven/plusSeven/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plusSeven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = 5;
+            int addedNumber = 7;
+            int stopNumber = 100;
+
+            for (int i = startingNumber; i <= stopNumber; i += addedNumber)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
